1d4f6c28dff562f63e20ca69b4975207a297de63
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-10-16 05:07:48 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main - main block
-*return: 0
+* main - Entry point of the program
+*
+* Return: Always 0 (success)
 */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
