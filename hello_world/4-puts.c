dd5c702449d0a80c4a7deaa38db4cf793a31bbc6
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-10-16 03:38:15 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-char* str1 = "Programming is like building a multilingual puzzle"|  puts(str1)
+#include <stdio.h>
+/**
+ *main - main point
+ *return: 0 
+ */
+int main(void)
+{
+    puts("\"programming is like building a multilingual puzzle\n");
+    return (0);
+}   
