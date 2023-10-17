b1f2800ccd0591033c3f5b3abfad3d22f0f8e256
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-10-17 06:34:41 -0700

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,16 +7,11 @@
 */
 int main(void)
 {
-int x;
-for (x = 48; x < 58; x++)
+int i;
+for (i = 0; i < 10; i++)
 {
-putchar(x);
-if (x < 57)
-{
-putchar(44);
-putchar(32);
+printf("%d", i);
 }
-}
-putchar('\n');
+printf("\n");
 return (0);
 }
