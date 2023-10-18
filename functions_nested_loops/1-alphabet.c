1c6e03de4706d86c4fa25fdd40bb8eba3302fd09
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-10-18 05:55:16 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,6 @@
+/**
+*File:main.h
+*Author:Pericles Adjovi
+*Desc:header file containing functions declaration
+*/
 void print_alphabet(void);
