26e02e51ea1873373bc248e330b8502abd9d6602
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-10-18 06:50:34 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+* print_alphabet - prints the alphabet, in lowercase, followed by a new line
 */
 void print_alphabet(void)
 {
-int letter;
-for (letter = 'a'; letter <= 'z'; letter++)
+char ch;
+ch = 'a';
+while (ch <= 'z')
 {
-_putchar (letter);
+_putchar(ch);
+ch++;
 }
-_putchar (10);
+_putchar('\n');
 }
