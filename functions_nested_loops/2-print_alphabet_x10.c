b5fdb9a240ddc11630d1f95ddb79a294b50071f7
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-10-19 03:03:58 -0700

    2-alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_alphabet_x10 - prints the alphabet in lowercase ten times, followed by a new line
+* print_alphabet_x10 - prints the alphabet in lowercase ten times
 */
 void print_alphabet_x10(void)
 {
