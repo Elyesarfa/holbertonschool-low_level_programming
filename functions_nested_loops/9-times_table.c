80111adcec1515d17604578207eccb7829db3788
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-10-21 07:44:49 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,10 +5,13 @@
 void times_table(void)
 {
 int i, j, result;
-for (i = 0; i <= 9; i++) {
-for (j = 0; j <= 9; j++) {
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
 result = i * j;
-if (j > 0) {
+if (j > 0)
+{
 _putchar(',');
 _putchar(' ');
 if (result < 10)
@@ -16,7 +19,8 @@ _putchar(' ');
 }
 if (result < 10)
 _putchar(result + '0');
-else {
+else
+{
 _putchar(result / 10 + '0');
 _putchar(result % 10 + '0');
 }
