878f85ac6cc72d9fcb67026ed0a9e6b681c2d7ed
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-10-28 11:23:23 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,22 +8,27 @@
 */
 char *cap_string(char *str)
 {
-char *ptr = str;
-int capitalize = 1;
-while (*ptr != '\0')
+int index = 0;
+while (dtr[index])
 {
-if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ',' ||
-*ptr == ';' || *ptr == '.' || *ptr == '!' || *ptr == '?' ||
-*ptr == '"' || *ptr == '(' || *ptr == ')' || *ptr == '{' || *ptr == '}')
-{
-capitalize = 1;
-}
-else if (capitalize && *ptr >= 'a' && *ptr <= 'z')
-{
-*ptr -= 32;
-capitalize = 0;
-}
-ptr++;
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
+index == 0)
+str[index] -= 32;
+index++;
 }
 return (str);
 }
