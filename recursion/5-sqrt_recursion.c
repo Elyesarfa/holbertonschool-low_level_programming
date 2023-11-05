dac74caaaa284d093afb8445580b181deecc94e3
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-11-05 10:32:37 -0800

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,10 +1,24 @@
 #include "main.h"
 /**
-* _sqrt_recursion - recursive power function
+* calculate_sqrt - Helper function to calculate the square root using recursion
+* @n: The number for which to calculate the square root
+* @guess: The current guess for the square root
 *
-* @n: integer to find the square root of
+* Return: The square root of n if found, -1 otherwise
+*/
+int calculate_sqrt(int n, int guess)
+{
+if (guess * guess == n)
+return (guess);
+if (guess * guess > n)
+return (-1);
+return (calculate_sqrt(n, guess + 1));
+}
+/**
+* _sqrt_recursion - Main function to calculate the square root of a number
+* @n: The number for which to calculate the square root
 *
-* Return: the natural square root of n
+* Return: The square root of n if found, -1 otherwise
 */
 int _sqrt_recursion(int n)
 {
@@ -12,8 +26,5 @@ if (n < 0)
 return (-1);
 if (n == 0 || n == 1)
 return (n);
-if (n % 2 == 0)
-return (_sqrt_recursion(n / 2) * _sqrt_recursion(n / 2));
-else
-return (n / 2 + _sqrt_recursion(n / 2 + 1) * _sqrt_recursion(n / 2 + 1));
+return (calculate_sqrt(n, 1));
 }
