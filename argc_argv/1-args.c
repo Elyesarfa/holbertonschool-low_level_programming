695affcf5a1e1c88245aba573008cfd82c2c3273
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-11-07 02:31:44 -0800

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,12 +1,7 @@
 #include<stdio.h>
 #include"main.h"
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-int i;
-printf("%d\n",argc);
-for(i=1;i<argc;i++)
-{
-printf("%s",argv[i]);
-}
+printf("%d\n",argc -1);
 return 0;
 }
