b4dc71f8bfb9649be1c6d38b48b47ee78a0ae085
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-11-12 10:36:30 -0800

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -8,15 +8,17 @@
 */
 char *_strdup(char *str)
 {
+char *abc;
+int i, r = 0;
 if (str == NULL)
 return (NULL);
-int len = 0;
-while (str[len] != '\0')
-len++;
-char *duplicate = (char *)malloc(sizeof(char) * (len + 1));
-if (duplicate == NULL)
+i = 0;
+while (str[i] != '\0')
+i++;
+abc = malloc(sizeof(char) * (i + 1));
+if (abc == NULL)
 return (NULL);
-for (int i = 0; i <= len; i++)
-duplicate[i] = str[i];
-return (duplicate);
+for (r = 0; str[r]; r++)
+abc[r] = str[r];
+return (abc);
 }
