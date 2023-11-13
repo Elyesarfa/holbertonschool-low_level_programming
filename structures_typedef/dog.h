9fad6e57508f09122863380ed6aa9f8680fe43ad
Elyesarfa <Elyesarfa@users.noreply.github.com>
2023-11-13 03:36:56 -0800

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_G
+#define DOG_G
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
+#endif
